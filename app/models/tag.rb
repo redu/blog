9d7f73c95d5aaa406c675eba82a1ddbf363c0cec
Matheus Santana <matheus@redu.com.br>
2012-01-19 10:36:25 -0300

    Validação do tamanho do nome de tags (modelo tag)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,9 @@
 class Tag < ActiveRecord::Base
-  has_and_belongs_to_many :posts
+
+	has_and_belongs_to_many :posts
+	
+	# verifica se foi informado um nome para a tag
+	# TODO confirmar o intervalo aceitável para o tamanho do nome
+	validates :name, :presence => true, :length => 1..30
+
 end
