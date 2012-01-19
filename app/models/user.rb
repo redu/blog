f49686fa0f6dd3b9843be05281a24f3f24f063bb
Matheus Santana <matheus@redu.com.br>
2012-01-19 10:01:19 -0300

    Validação de e-mail e senha no modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  has_many :posts
+	has_many :posts
+
+	# verifica se foi informado um email para o usuario
+	# possivelmente validates_format_of invalidaria um email inexistente
+	# portanto, mantive o codigo abaixo apenas para finalidade de informação
+	validates :email, :presence  => true
+
+	# valida o formato de email
+	validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :on => :create
+
+	# verifica a existência de uma senha de usuário e valida seu tamanho
+	# TODO confirmar o tamanho mínimo aceitável para senha
+	validates :password, 	:presence => true,
+							:length => { :minimum => 5 }
 end
