c65f19c8911f035fc9c68022af2c34330bce118a
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-01-23 17:36:09 -0300

    implementação parcial busca

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,39 @@ class Post < ActiveRecord::Base
 
 	# TODO viabilizar a validação da associação post-user (usuário deve existir)
 
+
+    def self.search query
+      result_posts = []
+
+      Post.all.collect do |post|
+        Post.relevant_terms(query).collect do |term|
+          if post.body.downcase.include? term.downcase or post.title.downcase.include? term.downcase
+            result_posts << post
+          end
+        end
+      end
+      result_posts
+    end
+
+    def self.relevant_terms query
+      query.strip!
+      if query.include? ','
+        query.gsub!(',', ' ')
+      end
+      term_list = query.split.collect! {|term| term.strip}
+      Post.remove_stop_words(term_list)
+    end
+
+    def self.remove_stop_words term_list
+      relevant_terms = []
+      stop_words = Blog::Application.config.stop_words
+
+      term_list.collect do |term|
+        unless stop_words.include? term
+          relevant_terms << term
+        end
+      end
+      relevant_terms
+    end
+
 end
