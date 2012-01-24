3f999fe298f5f460fe2eb7fc4bd62be7c593490b
Filipe Varjão <varjaofilipe@gmail.com>
2012-01-24 16:53:01 -0300

    RSS na pagina de post, detalhe na criação do feed.rss.builder dentro de views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,6 +52,14 @@ class PostsController < ApplicationController
       end
     end
 
+	def feed
+		@posts = Post.order
+		respond_to do |format|
+			format.rss {render :layout => false } #feed.rss.builder
+			format.json {renter :layout => false}
+		end
+	end
+
     # FIXME: melhorar busca!
     def search
       @posts = Post.all.collect.find_all{|post| post.body.downcase.include? params[:search].downcase or post.title.downcase.include? params[:search].downcase}
