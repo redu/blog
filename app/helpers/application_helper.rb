6daed1a5b08a046b76d4fc91a72e03f6ad0502d7
Matheus Santana <matheus@redu.com.br>
2012-01-26 16:29:50 -0300

    Refatoração do código da view index que verifica existência de usuário e criação de usuários com description (agora funcionando).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def getUsername(post)
+		if post.user == nil
+			poster = 'Desconhecido'
+		else
+			poster = post.user.username
+		end
+	end
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :trackable, :recoverable, :registerable, :rememberable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :description
 	
 	has_many :posts
 
