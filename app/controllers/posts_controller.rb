4ece8ff6b5fa25cf85f4dbe0f3dbef6269d24a0e
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-01-31 15:15:42 -0300

    closes #10

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_filter :authenticate_user!, :except => [:index, :show, :search, :search_tags]
+  before_filter :authenticate_user!, :except => [:index, :show, :search, :search_tags, :archive]
 
   def index
     @posts = Post.all
