97f04f2d6d066f7faa08227955668a8783d1cb67
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-02-07 19:16:03 -0300

    implementação parcial da integração com o blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-	def getAuthor(post)
-		if post.user == nil
-			author = 'Desconhecido'
-		else
-			author = link_to(post.user.username, post.user.profile_link)
-		end
-		author
-	end
-	
+  def getAuthor(post)
+    if post.user == nil
+      author = 'Desconhecido'
+    else
+      author = link_to(post.user.username, post.user.profile_link, :class => "link_author")
+    end
+    author
+  end
+
 end
