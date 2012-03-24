2efcaedd9c2d1baf0ae17a95c33065b7907ea5d3
Matheus Santana <matheus@redu.com.br>
2012-03-24 11:35:19 -0300

    Atualização de configuração do ambiente de produção I18N.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,17 @@ Blog::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+  
+  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+ config.time_zone = 'Brasilia'
+
+  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+  config.i18n.default_locale = "pt-BR"
+
+  # Configure the default encoding used in templates for Ruby 1.9.
+  config.encoding = "utf-8"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
